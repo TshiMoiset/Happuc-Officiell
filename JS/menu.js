44387af993ecc172965925069c2fc6a16ea8f5de
TshiMoiset <71385862+TshiMoiset@users.noreply.github.com>
2024-03-25 22:59:33 +0100

    Blur Done :fire:

--- JS/menu.js
+++ JS/menu.js
@@ -1,9 +1,13 @@
 function openNavbar() {
   document.getElementById("hamburgerMenu").style.width = "25%";
-  document.getElementById("main-content").classList.add("blur"); // Add blur class
+  document.getElementById("blurEffect").classList.add("blur"); // Add blur class
+  document.getElementById("blurEffect2").classList.add("blur"); // Add blur class
+  document.getElementById("blurEffect3").classList.add("blur"); // Add blur class
 }
 
 function closeNavbar() {
   document.getElementById("hamburgerMenu").style.width = "0";
-  document.getElementById("main-content").classList.remove("blur"); // Remove blur class
+  document.getElementById("blurEffect").classList.remove("blur"); // Remove blur class
+  document.getElementById("blurEffect2").classList.remove("blur"); // Remove blur class
+  document.getElementById("blurEffect3").classList.remove("blur"); // Remove blur class
 }
