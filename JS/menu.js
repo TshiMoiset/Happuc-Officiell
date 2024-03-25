ba2166a697a03d3fcc4f3d1423cb2031b23c2e38
TshiMoiset <71385862+TshiMoiset@users.noreply.github.com>
2024-03-25 22:34:05 +0100

    Blur

--- JS/menu.js
+++ JS/menu.js
@@ -1,36 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Open the navbar
-  function openNavbar() {
-    document.getElementById("hamburgerMenu").style.width = "25%";
-  }
+function openNavbar() {
+  document.getElementById("hamburgerMenu").style.width = "25%";
+  document.getElementById("main-content").classList.add("blur"); // Add blur class
+}
 
-  // Close the navbar
-  function closeNavbar() {
-    document.getElementById("hamburgerMenu").style.width = "0";
-  }
-
-  // Function to handle clicks outside of the navbar
-  function closeOnOutsideClick(event) {
-    const navbar = document.getElementById("hamburgerMenu");
-    const openButton = document.getElementById("openNavbar");
-    if (
-      event.target !== navbar &&
-      event.target !== openButton &&
-      !navbar.contains(event.target)
-    ) {
-      closeNavbar();
-      document.removeEventListener("click", closeOnOutsideClick);
-    }
-  }
-
-  // Add event listeners to open and close the navbar
-  document
-    .getElementById("openNavbar")
-    .addEventListener("click", function (event) {
-      openNavbar();
-      // Add event listener to close the navbar when clicking outside of it
-      document.addEventListener("click", closeOnOutsideClick);
-    });
-
-  document.getElementById("closeNavbar").addEventListener("click", closeNavbar);
-});
+function closeNavbar() {
+  document.getElementById("hamburgerMenu").style.width = "0";
+  document.getElementById("main-content").classList.remove("blur"); // Remove blur class
+}
