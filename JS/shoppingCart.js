50e4df67d77891bd239da8465bdc2d7ece0c2f8a
TshiMoiset <71385862+TshiMoiset@users.noreply.github.com>
2024-03-28 00:34:24 +0100

    Shopping cart

---
+++ JS/shoppingCart.js
@@ -0,0 +1,19 @@
+function openShoppingCart() {
+  let screenWidth = window.innerWidth;
+
+  if (screenWidth < 426) {
+    document.getElementById("shoppingCart").style.width = "100%";
+  } else if (screenWidth < 769) {
+    document.getElementById("shoppingCart").style.width = "20%";
+  } else if (screenWidth < 1025) {
+    document.getElementById("shoppingCart").style.width = "20%";
+  } else {
+    document.getElementById("shoppingCart").style.width = "20%"; // Default width for larger screens
+  }
+  document.getElementById("blurEffect").classList.add("blur"); // Add blur class
+}
+
+function closeShoppingCart() {
+  document.getElementById("shoppingCart").style.width = "0%";
+  document.getElementById("blurEffect").classList.remove("blur"); // Remove blur class
+}
