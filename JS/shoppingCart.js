e87d9858cc1a97fb2fb08ea2d225cf56127101e3
TshiMoiset <71385862+TshiMoiset@users.noreply.github.com>
2024-03-29 18:39:15 +0100

    :fire:

--- JS/shoppingCart.js
+++ JS/shoppingCart.js
@@ -4,9 +4,9 @@ function openShoppingCart() {
   if (screenWidth < 426) {
     document.getElementById("shoppingCart").style.width = "100%";
   } else if (screenWidth < 769) {
-    document.getElementById("shoppingCart").style.width = "20%";
+    document.getElementById("shoppingCart").style.width = "35%";
   } else if (screenWidth < 1025) {
-    document.getElementById("shoppingCart").style.width = "20%";
+    document.getElementById("shoppingCart").style.width = "35%";
   } else {
     document.getElementById("shoppingCart").style.width = "20%"; // Default width for larger screens
   }
